59a40defc7636bf5631dddd7ed1548fcb194d3be
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-20 16:04:33 +0100

    add ticket price

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-const km = prompt("Quanti km vuoi percorrere?");
+//FIRST and SECOND STEP
+const km = prompt("Quanti km vuoi percorrere?",20);
 const age = prompt("Quanti anni hai?");
 
 console.log( km, age );
+
+//THIRD STEP
+let price = ( 0.21 * km );
+console.log( price)
+
+//FOURTH STEP
