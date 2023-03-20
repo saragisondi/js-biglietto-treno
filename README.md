e13d42c3332bde802c5734401debb3add61a2347
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-20 18:00:58 +0100

    add steps readme

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ L’output del prezzo finale va messo fuori in forma umana (con massimo due deci
 - Moltiplicare il prezzo del biglietto (0.21€ al km) per i chilometri che l'utente vuole percorrere;
 - Inserire le istruzioni per applicare le condizioni della scontistica;
 - Stampare in pagina l'output in forma umana;
+
+## First & Second Step
+- Ho fatto due prompt, uno per chiedere i km da percorrere e l'altro per chiedere l'età;
+
+## Third  Step
+- Ho moltiplicato il prezzo al km per i km inseriti dall'utente;
+
+## Fourth Step
+- Ho calcolato lo sconto moltiplicando la percentuale al prezzo del biglietto per km e inserito le variabili all'interno di due condizioni: età inferiore a 18 anni oppure età maggiore a 65 anni.
+
+## Fifth Step
+- 
--- js/script.js
+++ js/script.js
@@ -9,24 +9,23 @@ let price = ( 0.21 * km );
 console.log( price)
 
 //FOURTH STEP
-let outputMessage = "";
-
-//PRICE UNDER 18
-
-
-/*let under18discount = ( price - discount20 )
-console.log (under18discount);
-//PRICE OVER 65
-
-
-let over65discount = ( price - discount40 )
-console.log (over65discount);*/
 
 //DISCOUNT
 if (age < 18){
-  let discount20 = ( 20 * price / 100 );
+  let discount20 =( 20 * price / 100 );
   console.log( discount20 );
 }else if (age > 65){
   let discount40 = ( 40 * price / 100);
   console.log( discount40 );
 }
+
+//FINAL PRICE
+//FINAL PRICE UNDER 18
+/*if (price == discount20){
+  let under18discount = ( price - discount20 )
+console.log (under18discount);
+}
+
+//FINAL PRICE OVER 65
+let over65discount = ( price - discount40 )
+console.log (over65discount);*/
