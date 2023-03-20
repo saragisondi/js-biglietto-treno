c35b5d919c496fac75a7cb5ff2d2c0a61e093ecd
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-20 17:06:22 +0100

    add age discount

--- js/script.js
+++ js/script.js
@@ -9,3 +9,24 @@ let price = ( 0.21 * km );
 console.log( price)
 
 //FOURTH STEP
+let outputMessage = "";
+
+//PRICE UNDER 18
+
+
+/*let under18discount = ( price - discount20 )
+console.log (under18discount);
+//PRICE OVER 65
+
+
+let over65discount = ( price - discount40 )
+console.log (over65discount);*/
+
+//DISCOUNT
+if (age < 18){
+  let discount20 = ( 20 * price / 100 );
+  console.log( discount20 );
+}else if (age > 65){
+  let discount40 = ( 40 * price / 100);
+  console.log( discount40 );
+}
